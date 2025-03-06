ea2191eca3c19bbd68e90783d545141af65a6908
anadi <gammarayelectron@gmail.com>
2025-03-06 20:27:34 +0000

    starting the test

--- src/main/java/org/gammaraylab/Main.java
+++ src/main/java/org/gammaraylab/Main.java
@@ -1,9 +1,17 @@
  package org.gammaraylab;
 
+ import org.graalvm.nativeimage.PinnedObject;
+
  public class Main {
 
      public static void main(String[] args) throws Exception {
      TestCases tc= new TestCases();
      tc.passingJavaObj();
+//         int secretKey=2;
+//         ECC ecc= new ECC();
+//         Point generator=new Point(0,3);
+//         Point pub= ecc.scalarMultiplication(secretKey,generator);
+//         pub.printPoint();
      }
+
  }
