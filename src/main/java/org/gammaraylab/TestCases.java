ee55c6664aab2b0372b8d6ff52ce783c1a3649a5
anadi <gammarayelectron@gmail.com>
2025-03-06 20:27:04 +0000

    passing objects to python

--- src/main/java/org/gammaraylab/TestCases.java
+++ src/main/java/org/gammaraylab/TestCases.java
@@ -5,51 +5,58 @@ import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 import org.graalvm.polyglot.proxy.ProxyArray;
+import org.graalvm.polyglot.proxy.ProxyExecutable;
 import org.graalvm.polyglot.proxy.ProxyObject;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TestCases {
-    String VENV_EXECUTABLE= Paths.get(System.getProperty("user.home"),"docker-wd/IdeaProjects/Polyglot-examples",".venv","bin","python").toString();
+    String VENV_EXECUTABLE;
+    Context context = null;
+
+    public TestCases() {
+        VENV_EXECUTABLE = Paths.get(System.getProperty("user.home"), "docker-wd/IdeaProjects/Polyglot-examples", ".venv", "bin", "python").toString();
+    }
 
     void passingJavaObj() {
-        Context context= Context.newBuilder("python")
-                .allowAllAccess(true)
-                .option("python.Executable",VENV_EXECUTABLE)
-                .option("python.ForceImportSite","true")
+        context = Context.newBuilder("python")
+                .option("python.Executable", VENV_EXECUTABLE)
+                .option("python.ForceImportSite", "true")
                 .build();
-        try{
-            File file= new File("src/main/polyglot/halwa.py");
-            Map<String,String> roomMap= new HashMap<>();
-            roomMap.put("113", "anadi");
-            roomMap.put("110", "utkarsh");
-            roomMap.put("103", "david");
+        try {
+            List<Point> publicKeyList= new ArrayList<>();
 
-            Map<String,Object> obj= new HashMap<>();
-            obj.put("roomMap",roomMap);
-            H12 h12= new H12(obj);
-            Value pythonBindings= context.getBindings("python");
-            pythonBindings.putMember("testCases",new TestCases());   //setting the proxy object to python var
-            context.eval("python", "testCases.method")
-                    .execute(h12);
-//            context.eval(Source.newBuilder("python",file).build());
-        }
-        catch (Exception e) {
+            List<Point> generatorList= new ArrayList<>();
+            generatorList.add(new Point(72344,5426));
+            generatorList.add(new Point(63234,26));
+            generatorList.add(new Point(6234,56));
+            generatorList.add(new Point(82443,6709826));
+            generatorList.add(new Point(8464,437));
+            generatorList.add(new Point(234356,552));
+
+            List<Integer> secretKeyList= new ArrayList<>(Arrays.asList(2345,435,67,5485,2323,7643));
+            ProxyList<Point> generatorProxy = new ProxyList<>(generatorList);
+            ProxyList<Integer> secretKeyProxy = new ProxyList<>(secretKeyList);
+
+            File file = new File("src/main/polyglot/ecc.py");
+            Value pythonBindings = context.getBindings("python");
+            pythonBindings.putMember("Point", new Point());
+            context.eval(Source.newBuilder("python", file).build());
+            Value processList = pythonBindings.getMember("processList");
+            // calling processList and passing objects to it
+            Value result = processList.execute(generatorProxy, secretKeyProxy);
+
+            // Convert result back to Java list
+            for (int i = 0; i < result.getArraySize(); i++) {
+                Value pointValue = result.getArrayElement(i);
+                Point p = pointValue.as(Point.class);
+                System.out.println(p);
+            }
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    @FunctionalInterface
-    interface ProxyObjectProvider {
-        ProxyObject getObject();
-    }
-
-    @HostAccess.Export
-    public void method(Value o) {
-        H12 proxy = o.asProxyObject();
-        System.out.println(proxy.values); // access values
-    }
 }
